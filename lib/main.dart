2d5155616905f82ca954cbd036985553421847ef
mseptiawan <syiffamuqaddimah_2226240143@mhs.mdp.ac.id>
2024-12-05 21:26:29 +0700

    final - add checkout screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jamtangan/screens/cart_screen.dart';
+import 'package:jamtangan/screens/checkout_screen.dart';
 import 'package:jamtangan/screens/detail_screen.dart';
 import 'package:jamtangan/screens/favorit_screen.dart';
 import 'package:jamtangan/screens/home_screen.dart';
@@ -16,15 +17,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.brown,
-        ),
-        //home: SigninScreen()
-        // home: HomeScreen(),
-        // home : DetailScreen()
-        // home : ProfileScreen()
-        // home: CartScreen(),
-        home: FavoritScreen());
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      //home: SigninScreen()
+      // home: HomeScreen(),
+      // home : DetailScreen()
+      // home : ProfileScreen()
+      // home: CartScreen(),
+      home: CheckoutScreen(),
+      // home: FavoritScreen()
+    );
   }
 }
